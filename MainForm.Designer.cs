a68ba056748a7d08d614c84b3690743da18d5848
unknown <dpage88@gmail.com>
2019-03-10 05:23:08 -0500

    Moved from Bitbucket.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Oc
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this._mnuMainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._mnuDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._mnu50pct = new System.Windows.Forms.ToolStripMenuItem();
+            this._mnu100pct = new System.Windows.Forms.ToolStripMenuItem();
+            this._mnu200pct = new System.Windows.Forms.ToolStripMenuItem();
+            this.showAxisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._mnuBackgroundColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._txtInfoMessage = new System.Windows.Forms.TextBox();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this._mnuFullscreen = new System.Windows.Forms.ToolStripMenuItem();
+            this._mnuMainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _mnuMainMenu
+            // 
+            this._mnuMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this._mnuMainMenu.Location = new System.Drawing.Point(0, 0);
+            this._mnuMainMenu.Name = "_mnuMainMenu";
+            this._mnuMainMenu.Size = new System.Drawing.Size(384, 24);
+            this._mnuMainMenu.TabIndex = 1;
+            this._mnuMainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._mnuDelete});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // _mnuDelete
+            // 
+            this._mnuDelete.Name = "_mnuDelete";
+            this._mnuDelete.Size = new System.Drawing.Size(107, 22);
+            this._mnuDelete.Text = "Delete";
+            this._mnuDelete.Click += new System.EventHandler(this._mnuDelete_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._mnu50pct,
+            this._mnu100pct,
+            this._mnu200pct,
+            this.showAxisToolStripMenuItem,
+            this._mnuBackgroundColor,
+            this._mnuFullscreen});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // _mnu50pct
+            // 
+            this._mnu50pct.Name = "_mnu50pct";
+            this._mnu50pct.Size = new System.Drawing.Size(170, 22);
+            this._mnu50pct.Text = "50%";
+            this._mnu50pct.Click += new System.EventHandler(this._mnu50pct_Click);
+            // 
+            // _mnu100pct
+            // 
+            this._mnu100pct.Name = "_mnu100pct";
+            this._mnu100pct.Size = new System.Drawing.Size(170, 22);
+            this._mnu100pct.Text = "100%";
+            this._mnu100pct.Click += new System.EventHandler(this._mnu100pct_Click);
+            // 
+            // _mnu200pct
+            // 
+            this._mnu200pct.Name = "_mnu200pct";
+            this._mnu200pct.Size = new System.Drawing.Size(170, 22);
+            this._mnu200pct.Text = "200%";
+            this._mnu200pct.Click += new System.EventHandler(this._mnu200pct_Click);
+            // 
+            // showAxisToolStripMenuItem
+            // 
+            this.showAxisToolStripMenuItem.Name = "showAxisToolStripMenuItem";
+            this.showAxisToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.showAxisToolStripMenuItem.Text = "Show Axis";
+            this.showAxisToolStripMenuItem.Click += new System.EventHandler(this.showAxisToolStripMenuItem_Click);
+            // 
+            // _mnuBackgroundColor
+            // 
+            this._mnuBackgroundColor.Name = "_mnuBackgroundColor";
+            this._mnuBackgroundColor.Size = new System.Drawing.Size(170, 22);
+            this._mnuBackgroundColor.Text = "Background Color";
+            this._mnuBackgroundColor.Click += new System.EventHandler(this._mnuBackgroundColor_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As..";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // _txtInfoMessage
+            // 
+            this._txtInfoMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._txtInfoMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this._txtInfoMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this._txtInfoMessage.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this._txtInfoMessage.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._txtInfoMessage.Location = new System.Drawing.Point(97, 27);
+            this._txtInfoMessage.Multiline = true;
+            this._txtInfoMessage.Name = "_txtInfoMessage";
+            this._txtInfoMessage.ReadOnly = true;
+            this._txtInfoMessage.Size = new System.Drawing.Size(287, 72);
+            this._txtInfoMessage.TabIndex = 2;
+            this._txtInfoMessage.Text = "Information Message";
+            this._txtInfoMessage.Visible = false;
+            this._txtInfoMessage.MouseClick += new System.Windows.Forms.MouseEventHandler(this._txtInfoMessage_MouseClick);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._mnuAbout});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // _mnuAbout
+            // 
+            this._mnuAbout.Name = "_mnuAbout";
+            this._mnuAbout.Size = new System.Drawing.Size(152, 22);
+            this._mnuAbout.Text = "About";
+            this._mnuAbout.Click += new System.EventHandler(this._mnuAbout_Click);
+            // 
+            // _mnuFullscreen
+            // 
+            this._mnuFullscreen.Name = "_mnuFullscreen";
+            this._mnuFullscreen.Size = new System.Drawing.Size(170, 22);
+            this._mnuFullscreen.Text = "Fullscreen (F12)";
+            this._mnuFullscreen.Click += new System.EventHandler(this._mnuFullscreen_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 305);
+            this.Controls.Add(this._txtInfoMessage);
+            this.Controls.Add(this._mnuMainMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this._mnuMainMenu;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this._mnuMainMenu.ResumeLayout(false);
+            this._mnuMainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip _mnuMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _mnu50pct;
+        private System.Windows.Forms.ToolStripMenuItem _mnu100pct;
+        private System.Windows.Forms.ToolStripMenuItem _mnu200pct;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _mnuDelete;
+        private System.Windows.Forms.ToolStripMenuItem showAxisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _mnuBackgroundColor;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.TextBox _txtInfoMessage;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _mnuAbout;
+        private System.Windows.Forms.ToolStripMenuItem _mnuFullscreen;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using Vec2f = OpenTK.Vector2;
+using Vec3f = OpenTK.Vector3;
+namespace Oc
+{
+
+    public partial class MainForm : Form, IMessageFilter
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(Point p);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg, IntPtr wParam, IntPtr lParam);
+
+        EngineManager _objEngineManager;
+        NodeManager GetNodeManager() { return _objEngineManager.GetNodeManager(); }
+
+        public List<string> _objValidImageExtensions = new List<string>
+            {
+                ".jpg", ".bmp", ".png", ".gif"
+            };
+        public List<string> _objValidSaveExtensions = new List<string>
+            {
+                ".jpg", ".bmp", ".png"
+            };
+
+        public EngineManager GetEngineManager() { return _objEngineManager; }
+        public MainForm()
+        {
+            InitializeComponent();
+            Text = "Omicron Enhanced Viewer";
+        }
+
+        #region Private: GUI Callbacks
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _objEngineManager = new EngineManager(this);
+
+            GLControl glControl = _objEngineManager.GetGLControl();
+
+            glControl.Dock = DockStyle.Fill;
+
+            _objEngineManager.Initialize(true);
+
+            Application.AddMessageFilter(this);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path = GetValidUserFile();
+            LoadImage(path);
+        }
+        private void _mnu50pct_Click(object sender, EventArgs e)
+        {
+            if (GetNodeManager().SelectedNode == null)
+                return;
+
+            GetNodeManager().SelectedNode.SetPos(new Vec3f(0, 0, 0));
+            GetNodeManager().SelectedNode.SetScale(new Vec3f(0.5f, 0.5f, 0.5f));
+            //  _objRenderManager.Invalidate();
+        }
+        private void _mnu100pct_Click(object sender, EventArgs e)
+        {
+            if (GetNodeManager().SelectedNode == null)
+                return;
+
+            GetNodeManager().SelectedNode.SetPos(new Vec3f(0, 0, 0));
+            GetNodeManager().SelectedNode.SetScale(new Vec3f(1.0f, 1.0f, 1.0f));
+            // _objRenderManager.Invalidate();
+        }
+        private void _mnu200pct_Click(object sender, EventArgs e)
+        {
+            if (GetNodeManager().SelectedNode == null)
+                return;
+
+            GetNodeManager().SelectedNode.SetPos(new Vec3f(0, 0, 0));
+            GetNodeManager().SelectedNode.SetScale(new Vec3f(2.0f, 2.0f, 2.0f));
+            // _objRenderManager.Invalidate();
+        }
+        private void _mnuDelete_Click(object sender, EventArgs e)
+        {
+            _objEngineManager.GetNodeManager().DeleteSelectedNode();
+        }
+        private void showAxisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (GetEngineManager().GetRenderManager().ShowAxis == false)
+            {
+                GetEngineManager().GetRenderManager().ShowAxis = true;
+                showAxisToolStripMenuItem.Text = "Hide Axis";
+            }
+            else
+            {
+                GetEngineManager().GetRenderManager().ShowAxis = false;
+                showAxisToolStripMenuItem.Text = "Show Axis";
+            }
+        }
+        private void _mnuBackgroundColor_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ColorDialog cd = new System.Windows.Forms.ColorDialog();
+            DialogResult res = cd.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                GetEngineManager().GetRenderManager().SetBackgroundColor(new Vector4(
+                    (float)cd.Color.R / 255.0f,
+                    (float)cd.Color.G / 255.0f,
+                    (float)cd.Color.B / 255.0f,
+                    (float)cd.Color.A / 255.0f
+                    ));
+            }
+        }
+        #endregion
+
+        #region Private:Methods
+        private TextureLoadResult LoadImage(string path)
+        {  
+
+            if (path == string.Empty)
+                return TextureLoadResult.InvalidPath;
+
+            string ext = System.IO.Path.GetExtension(path).ToLower();
+
+            if (!_objValidImageExtensions.Contains(ext))
+            {
+                System.Windows.Forms.MessageBox.Show("The file type " + ext + " is not supported.");
+                return TextureLoadResult.InvalidExtension;
+            }
+
+            BaseImage img = new BaseImage(GetEngineManager().GetRenderManager(), GetEngineManager().GetNodeManager());
+
+            TextureLoadResult res = img.Load(path);
+            if (res == TextureLoadResult.TooLarge)
+            {
+                int tx = Gpu.GetMaxTextureSize();
+                ShowInfoMessage("This image is too large.  The maximum allowed image width/height is " + tx.ToString() + " pixels.");
+                _objEngineManager.GetNodeManager().DeleteNode(img);
+            }
+            else
+            {
+                GetNodeManager().DeleteSelectedNode();
+                GetNodeManager().SelectedNode = img;
+            }
+            
+            return res;
+            //_objImageList.Add(img);
+        }
+        private string GetValidUserFile()
+        {
+            System.Windows.Forms.OpenFileDialog dlg = new System.Windows.Forms.OpenFileDialog();
+            System.Windows.Forms.DialogResult res = dlg.ShowDialog();
+
+            if (res == System.Windows.Forms.DialogResult.Cancel)
+                return string.Empty;
+
+            if (!System.IO.File.Exists(dlg.FileName))
+                return string.Empty;
+
+            return dlg.FileName;
+        }
+        #endregion
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //foreach (BaseImage img in _objImageList)
+            //{
+            //    img.Free();
+            //}
+        }
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == 0x0100)//key down
+            {
+                int key = (int)m.WParam;
+                if (key == (int)System.Windows.Forms.Keys.F12)
+                {
+                    SwapFullscreen();
+                }
+                if (key == (int)System.Windows.Forms.Keys.Escape)
+                {
+                    if (_blnFullscreen)
+                        ExitFullscreen();
+                }
+            }
+            else if (m.Msg == 0x0200)//mouse move
+            {
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+                IntPtr hWnd = WindowFromPoint(pos);
+                if (hWnd != IntPtr.Zero && hWnd != m.HWnd && Control.FromHandle(hWnd) != null)
+                {
+                    SendMessage(hWnd, m.Msg, m.WParam, m.LParam);
+                    return true;
+                }
+            }
+            //else if (m.Msg == 0x0201) //mouse lbutton
+            else if (m.Msg == 0x020a)//mouse whele
+            {
+                // WM_MOUSEWHEEL, find the control at screen position m.LParam
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+                IntPtr hWnd = WindowFromPoint(pos);
+                if (hWnd != IntPtr.Zero && hWnd != m.HWnd && Control.FromHandle(hWnd) != null)
+                {
+                    SendMessage(hWnd, m.Msg, m.WParam, m.LParam);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //System.Windows.Threading.DispatcherTimer _objInfoMessageHideTimer;
+        #region Info Messages
+
+        private const int EM_GETLINECOUNT = 0xba;
+        [DllImport("user32", EntryPoint = "SendMessageA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern int SendMessage(int hwnd, int wMsg, int wParam, int lParam);
+
+        private void ShowInfoMessage(string msg)
+        {
+            _txtInfoMessage.Text = msg;
+            var numberOfLines = SendMessage(_txtInfoMessage.Handle.ToInt32(), EM_GETLINECOUNT, 0, 0);
+            _txtInfoMessage.Height = (_txtInfoMessage.Font.Height + 2) * numberOfLines;
+            _txtInfoMessage.Show();
+            //_objUpdateTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);//ASAP
+            //_objUpdateTimer.Tick += new EventHandler(EngineTick);
+            //_objUpdateTimer.Start();
+        }
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_objEngineManager.GetNodeManager().SelectedNode == null)
+            {
+                ShowInfoMessage("Please select an image to save.");
+                return;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.AddExtension = true;
+            sfd.DefaultExt = ".jpg";
+
+            //Create Filter.
+            string filter;
+            filter = "Image Files (";
+            string app = "";
+            foreach (string ext in _objValidSaveExtensions)
+            {
+                filter += app + "*" + ext;
+                app = ",";
+            }
+            filter += ")|";
+            app="";
+            foreach (string ext in _objValidSaveExtensions)
+            {
+                filter += app + "*" + ext;
+                app = ";";
+            }
+            sfd.Filter = filter;
+            DialogResult dr = sfd.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                string fileName = sfd.FileName;
+
+                BaseImage img = (BaseImage)_objEngineManager.GetNodeManager().SelectedNode;
+                if (!_objValidImageExtensions.Contains(System.IO.Path.GetExtension(fileName)))
+                    ShowInfoMessage("The image cannot be saved as the specified type.  Please specify a valid path.");
+                else if (img == null)
+                    ShowInfoMessage("Invalid object was selected.");
+                else if(img.GetTexture()==null)
+                    ShowInfoMessage("Error object had no texture.");
+                else if(img.GetTexture().GetGlId()==0)
+                    ShowInfoMessage("Error object's texture was never sent to the GPU.");
+                else if(!GL.IsTexture(img.GetTexture().GetGlId()))
+                    ShowInfoMessage("Error object was not a recognized OpenGL texture.");
+                else
+                {
+                    try
+                    {
+                        img.SaveToDisk(fileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        ShowInfoMessage("An error has Occurred: \r\n" + ex.ToString());
+                    }
+                }
+
+            }
+        }
+
+
+        private void _txtInfoMessage_MouseClick(object sender, MouseEventArgs e)
+        {
+            _txtInfoMessage.Hide();
+        }
+        #endregion
+
+        AboutForm af = new AboutForm();
+        private void _mnuAbout_Click(object sender, EventArgs e)
+        {
+            af.Show();
+        }
+
+        bool _blnFullscreen = false;
+        private void SwapFullscreen()
+        {
+            if (_blnFullscreen)
+                ExitFullscreen();
+            else
+                EnterFullscreen();
+        }
+        private void EnterFullscreen()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            _mnuMainMenu.Visible = false;
+            _blnFullscreen = true;
+        }
+        private void ExitFullscreen()
+        {
+            _mnuMainMenu.Visible = true;
+            WindowState = FormWindowState.Normal;
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            _blnFullscreen = false;
+        }
+        private void _mnuFullscreen_Click(object sender, EventArgs e)
+        {
+            EnterFullscreen();
+        }
+    }
+}
